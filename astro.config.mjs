8d3988731681bff4188e5bcff9853ba74f7ce22c
Simon Wiles <simonjwiles@gmail.com>
2022-07-18 16:22:29 -0700

    Add support for direct import of YAML files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from "astro/config";
 
+import yaml from "@rollup/plugin-yaml";
+
 export default defineConfig({
   site: "https://sul-cidr.github.io/",
   base: "/cidr-site/",
   trailingSlash: "always",
+  vite: {
+    plugins: [yaml()],
+  },
 });
